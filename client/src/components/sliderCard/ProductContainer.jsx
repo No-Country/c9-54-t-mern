48d0fda326345ce4fa5d6663434742e26a9071bd
JorgeDev <espinozadiazjorgeluis@gmail.com>
2023-02-09 18:09:27 -0500

    add components card

--- client/src/components/sliderCard/ProductContainer.jsx
+++ client/src/components/sliderCard/ProductContainer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ProductContainer = (props) => {
   return (
-    <section className="bg-slate-500 w-11/12 m-auto h-full product__container flex gap-4 ">
+    <section className="bg-[#EBEBEB] w-[100%]  h-auto flex flex-wrap justify-center items-center">
       {props.children}
     </section>
   );
