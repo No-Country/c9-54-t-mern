dba311e721f993099ce67a75d1ccc1a1c4f50c9a
Eitan <eitan03@hotmail.com.ar>
2023-02-15 17:18:13 -0300

    filter bar function

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { all } from "axios";
 import React, { useEffect, useState } from "react";
 import FilterBar from "../components/filterBar/filterBar";
 import Footer from "../components/footer/Footer";
@@ -8,28 +8,44 @@ import ProductsCard from "../components/sliderCard/ProductsCard";
 
 const Home = () => {
   const [products, setProducts] = useState();
+  const [filter, setFilter] = useState("all");
+  const [filteredProducts, setFilteredProducts] = useState();
+
+  const updateFilter = (filter) => {
+      setFilter(filter)
+  }
+
+  useEffect(() => {
+    if(filter === "all"){
+      setFilteredProducts(products)
+    } else{
+      setFilteredProducts(products.filter(product => product.productType === filter));
+    }
+  }, [filter]);
 
   useEffect(() => {
     axios
       .get("https://tudestinoapp-production.up.railway.app/api/products")
       .then((res) => {
         setProducts(res.data);
+        setFilteredProducts(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
+
   return (
     <div className="h-full flex flex-col justify-between">
       <div>
         <Slider />
         <div className="mt-3 border-b-2">
-          <FilterBar />
+          <FilterBar updateFilter={updateFilter}/>
         </div>
         <div className="container mx-auto">
           <ProductContainer>
-            {products?.map((prod, index) => (
+            {filteredProducts?.map((prod, index) => (
               <ProductsCard key={index} image={prod.image} data={prod} />
             ))}
           </ProductContainer>
