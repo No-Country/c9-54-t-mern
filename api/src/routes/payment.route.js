f0931bbfe9e6c7f9ed4ba06715393d37963ff236
dev-back55 <ahabitu@gmail.com>
2023-02-23 15:07:50 -0300

    payment-router-controller-validations

--- api/src/routes/payment.route.js
+++ api/src/routes/payment.route.js
@@ -2,19 +2,18 @@ import express from "express";
 import {
   createPayment,
   getAllPayment,
-  getPaymentId
+  getPaymentId,
+  getQtyPayment,
+  getTotalPayment
 } from "../controllers/payment.controller.js";
 
-
-
-
-
 const router = express.Router();
-
 
 router.get("/",getAllPayment);
 router.post("/",createPayment);
 router.get("/:id",getPaymentId);
+router.get("/count/count",getQtyPayment);
+router.get("/sum/totalpayment", getTotalPayment)
 
 export default router;
 
