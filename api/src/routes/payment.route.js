866ba3fd8360272f6b59b9db7cae3b61d3d102f9
dev-back55 <ahabitu@gmail.com>
2023-02-26 16:39:37 -0300

    Feact/paypal payment implementation

--- api/src/routes/payment.route.js
+++ api/src/routes/payment.route.js
@@ -6,12 +6,14 @@ import {
   getQtyPayment,
   getTotalPayment,
   buscarReservasPorFechaDeHoy,
+  savePayment,
 } from "../controllers/payment.controller.js";
 
 const router = express.Router();
 
 router.get("/",getAllPayment);
 router.post("/",createPayment);
+router.post("/save", savePayment);
 router.get("/:id",getPaymentId);
 router.get("/reservas/hoy", buscarReservasPorFechaDeHoy)
 router.get("/count/count",getQtyPayment);
